5952c3b2ab08bca5fca0f0e83e2d670e010b97c3
caihaiyang2 <caihaiyang2@gome.com.cn>
2021-08-08 13:52:19 +0800

    code

---
+++ UtilTool.h
@@ -0,0 +1,16 @@
+//
+//  UtilTool.h
+//  FERBase
+//
+//  Created by caihaiyang on 2021/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UtilTool : NSObject
+- (void)logData:(NSString *)text;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UtilTool.m
@@ -0,0 +1,14 @@
+//
+//  UtilTool.m
+//  FERBase
+//
+//  Created by caihaiyang on 2021/8/8.
+//
+
+#import "UtilTool.h"
+
+@implementation UtilTool
+- (void)logData:(NSString*)text{
+    NSLog(@"value = %@", text);
+}
+@end
